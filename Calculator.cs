2e7f4d0f856d0132408e2b4fefe450efb66fbec5
aryalksnaa <aryalpdewa16@students.amikom.ac.id>
2025-03-11 14:10:46 +0700

    melengkapi code

--- Calculator.cs
+++ Calculator.cs
@@ -2,12 +2,19 @@ class Calculator
 {
 	static void Main(string[]args)
 	{
-
+		int a = 10;
+		int b = 6;
+		
+		Console.WirteLine("Hasil penambahan: {0} + {1} + {2}", a, b, penambahan(a,b);
+		Console.WirteLine("Hasil pengurangan: {0} + {1} + {2}", a, b, pengurangan(a,b);
+		
+		Console.WirteLine("\nTekan sembarang key untuk keluar");
+		Console.Readkey();
 	}
 	
-	static int Penambahan (int a, intb)
+	static int Penambahan (int a, int b)
 	{
-		return a+b;
+		return a + b;
 	}
 	
 	static int pengurangan (int a, int b)
